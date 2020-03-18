9cf7f642a589d7b2e82b9448ac82489279f0871e
Natalia Dorozynska <natala.dorozynska@gmail.com>
2020-03-18 14:19:31 +0100

    tutorial #22

---
+++ FamilyCalendar/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using FamilyCalendar.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+
+        public ViewResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            return View("Test");
+        }
+    }
+}
---
+++ FamilyCalendar/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyCalendar.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() {Id = 1, Name = "Jone", Department = "HR", Email = "jone@gmail.com" },
+                new Employee() {Id = 2, Name = "Mary", Department = "IT", Email = "mary@gmail.com" },
+                new Employee() {Id = 3, Name = "Clara", Department = "IT", Email = "clara@gmail.com" },
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
