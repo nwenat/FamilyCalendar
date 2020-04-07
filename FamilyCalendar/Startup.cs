79b029412d1d930d14de0ac4ce541e83c6c9884c
Natalia Dorożyńska <natala.dorozynska@gmail.com>
2020-04-07 20:36:26 +0200

    Change Data Base

--- FamilyCalendar/Startup.cs
+++ FamilyCalendar/Startup.cs
@@ -26,7 +26,7 @@ namespace FamilyCalendar
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConection")));
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("CalendarDBConection")));
 
             services.AddIdentity<IdentityUser, IdentityRole>(options =>
             {
